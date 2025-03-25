ed17e741c53244bc638950a9d27a2a8f51abc3d7
oishi1996 <oishi.ims@gmail.com>
2025-03-26 07:05:39 +0900

    Remove deprecated pKa Predictor and 3D Molecular Viewer files; add new implementations for both features

--- pages/1_3D_Molecular_Viewer.py
+++ pages/1_3D_Molecular_Viewer.py
@@ -1,11 +1,9 @@
 import streamlit as st
 import py3Dmol
-import io
-import base64
-from PIL import Image
 from rdkit import Chem
 from rdkit.Chem import AllChem
 from stmol import showmol
+from streamlit_ketcher import st_ketcher
 
 # 3D分子構造を取得する関数
 def get_optimized_3Dmol(smiles, view_style):
@@ -27,22 +25,22 @@ def get_optimized_3Dmol(smiles, view_style):
     view.setStyle({view_style: {}})
     return mol, view
 
-# 画像を保存する関数
-def save_3Dmol_image(view):
-    img_data = view.png()  # py3Dmol の画像を取得
-    # img = Image.open(io.BytesIO(img_data))
-    
-    # 画像を一時保存
-    # img_path = "molecule.png"
-    # img_data.save(img_path)
-    return img_data
+# Streamlit UI
+st.title(":material/Science: 3D Molecular Viewer")
+st.markdown("""
+            - This app allows you to draw chemical structures and generate 3D structure
+            - Draw your molecule below...
+            """)
 
-# Streamlit アプリの設定
-st.title("3D Molecular Viewer")
+# --- SMILES 入力方法の選択 ---
+input_mode = st.radio("Choose input method:", ["Text Input", "Draw Molecule"], horizontal=True)
 
-# 入力欄
-smiles = st.text_input("Enter a SMILES string:", 
-    "COc3nc(OCc2ccc(C#N)c(c1ccc(C(=O)O)cc1)c2P(=O)(O)O)ccc3C[NH2+]")
+if input_mode == "Text Input":
+    smiles = st.text_input("Enter a SMILES string:", 
+        "COc3nc(OCc2ccc(C#N)c(c1ccc(C(=O)O)cc1)c2P(=O)(O)O)ccc3C[NH2+]")
+else:
+    smiles = st_ketcher()
+
 
 # 表示スタイル選択
 view_style = st.selectbox("Select a view style:", ["stick", "line", "sphere"])
@@ -52,24 +50,6 @@ if smiles:
     if mol:
         showmol(view, height=500, width=800)
         st.text("3D structure generated successfully.")
-        # 画像表示
-        view.png()
-
-        # 画像保存ボタン
-        if st.button("Save 3D Molecule Image"):
-            img_data = save_3Dmol_image(view)
-            img_data.type()
-
-            # 画像を表示
-            # st.image(img_path, caption="Saved 3D Molecule", use_column_width=True)
-
-            # ダウンロードボタン
-            # st.download_button(
-            #     label="Download Image",
-            #     data=img_data,
-            #     file_name="molecule.png",
-            #     mime="image/png"
-            # )
     else:
         st.error("3D structure generation failed. Please check the SMILES string.")
 
